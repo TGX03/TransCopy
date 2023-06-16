1b99c1d2b13675f9f5f87b10bcfb0eba0237dd43
tgx003 <tgx003@gmail.com>
2023-06-16 14:58:54 +0200

    Initial commit

---
+++ src/de/tgx03/StreamNuller.java
@@ -0,0 +1,31 @@
+package de.tgx03;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * A class used to empty a string in a separate thread without actually doing something with the output
+ */
+public class StreamNuller extends Thread {
+
+    /**
+     * A thread object that empties the buffer of a given InputStream
+     * to make sure the program doesn't get blocked as the buffer fills up.
+     * @param input The input stream to empty.
+     */
+    public StreamNuller(InputStream input) {
+        super(() -> {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+            try {
+                while (reader.readLine() != null) {
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
+        super.setDaemon(true);
+        super.setPriority(Thread.MIN_PRIORITY);
+    }
+}
