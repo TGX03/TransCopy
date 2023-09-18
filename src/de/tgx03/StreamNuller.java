e21e81c32d812523c631b9f78eb929b1eb643b13
tgx003 <tgx003@gmail.com>
2023-09-18 23:40:41 +0200

    StreamNuller now overrides the run method

--- src/de/tgx03/StreamNuller.java
+++ src/de/tgx03/StreamNuller.java
@@ -10,22 +10,40 @@ import java.io.InputStreamReader;
 public class StreamNuller extends Thread {
 
 	/**
+	 * The stream this is supposed to clear.
+	 */
+	private final InputStream stream;
+	/**
+	 * Used to make sure this Nuller doesn't run twice.
+	 * As this isn't atomic, this isn't a perfect solution, but should suffice if used sensibly.
+	 */
+	private volatile boolean running = false;
+
+	/**
 	 * A thread object that empties the buffer of a given InputStream
 	 * to make sure the program doesn't get blocked as the buffer fills up.
 	 *
 	 * @param input The input stream to empty.
 	 */
 	public StreamNuller(InputStream input) {
-		super(() -> {
-			InputStreamReader reader = new InputStreamReader(input);
-			char[] buffer = new char[1024];
-			try {
-				while (reader.read(buffer) != -1) ;
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		});
+		stream = input;
 		super.setDaemon(true);
 		super.setPriority(Thread.MIN_PRIORITY);
+	}
+
+	/**
+	 * Empties the provided input stream.
+	 */
+	@Override
+	public void run() {
+		if (running) return;
+		running = true;
+		InputStreamReader reader = new InputStreamReader(stream);
+		char[] buffer = new char[1024];
+		try {
+			while (reader.read(buffer) != -1) ;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
